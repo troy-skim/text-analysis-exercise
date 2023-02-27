92e364784f84f78787244a97dc44dd016a533953
Troy Kim <troy.skim@nyu.edu>
2023-02-26 22:28:20 -0500

    Almost Finished Assignment

--- src/edu/nyu/cs/App.java
+++ src/edu/nyu/cs/App.java
@@ -27,23 +27,41 @@ public class App {
    * @param args An array of any command-line arguments.
    */
   public static void main(String[] args) throws Exception {
-
+    
     // complete this function according to the instructions
-
+    System.out.println("What file would you like to open?");
+    String filePath = getFilepathFromUser();
+    String fullText = getContentsOfFile(filePath);
+    System.out.println("What words would you like to search for?");
+    String[] tics = getTicsFromUser();
+    System.out.println("\n...............................Analyzing text.................................\n");
+    int count = 0;
+    for (int i=0; i<tics.length; i++) {
+      int temp = countOccurrences(tics[i], fullText);
+      count += temp;
+    }
+    double density = calculateTicDensity(tics, fullText);
+    System.out.println("Total number of tics: "+count);
+    System.out.println("Density of tics: "+density);
+    System.out.println("\n...............................Tic breakdown..................................\n");
+    for (int j=0; j<tics.length; j++) {
+      System.out.println(String.format("%10s", tics[j].toLowerCase())+"/ "+ String.format("$19s", countOccurrences(tics[j], fullText)+" occurrences") + "/ "+calculatePercentage(countOccurrences(tics[j], fullText), count)+"% of all tics");
+    }
   }
 
   /**
    * getFilepathFromUser method
    * Asks the user to enter the path to the text file they want to analyze.
    * Hint:
-   *  - use the "global"-ish Scanner variable scn to get the response from the user, rather than creating a new Scanner variable ithin this function.
+   *  - use the "global"-ish Scanner variable scn to get the response from the user, rather than creating a new Scanner variable within this function.
    *  - do not close the "global"-ish Scanner so that you can use it in other functions
    * @return The file path that the user enters, e.g. "data/trump_speech_010621.txt"
    */
   public static String getFilepathFromUser() {
 
     // complete the getFilepathFromUser function according to the instructions above
-
+    String fileName = scn.nextLine();
+    return fileName;
   }
 
 
@@ -84,9 +102,12 @@ public class App {
    *  - do not close the "global"-ish Scanner so that you can use it in other functions
    * @return A String array containing each of the tics to analyze, with any leading or trailing whitespace removed from each tic.
    */
-
+  public static String[] getTicsFromUser() {
     // write the getTicsFromUser function according to the instructions
-
+    String ticString = scn.nextLine();
+    String[] tics = ticString.split("\\s*,\\s*");
+    return tics;
+  }
 
  /**
    * countOccurrences method
@@ -95,9 +116,20 @@ public class App {
    * @param haystack The String within which to search.
    * @return THe number of occurrences of the "needle" String within the "haystack" String, ignoring case.
    */
-
+  public static int countOccurrences(String needle, String haystack) {
     // write the countOccurrences function according to the instructions
-
+    haystack = haystack.toLowerCase();
+    needle = needle.toLowerCase();
+    // use the same method as the second test to create array and iterate through it
+    String[] haystackAry = haystack.split("[ \n\t.,?!]");
+    int count = 0;
+    for (int i=0; i<haystackAry.length; i++) {
+      if (haystackAry[i].equals(needle)) {
+        count += 1;
+      }
+    }
+    return count;
+  }
   /**
    * calculatePercentage method
    * Calculates the equivalent percentage from the proportion of one number to another number.
@@ -105,7 +137,10 @@ public class App {
    * @param num2 The overall number out of which the num1 number is taken.  i.e. the denominator in the ratio of num1 to num2.
    * @return The percentage that rum1 represents out of the total of num2, rounded to the nearest integer.
    */
-
+  public static int calculatePercentage(int num1, int num2) {
+    int rounded = (int) Math.round(100*num1/num2);
+    return rounded;
+  }
     // write the calculatePercentage function according to the instructions above
 
 
@@ -120,7 +155,21 @@ public class App {
    * @param fullText The full text.
    * @return The proportion of the number of tic words present in the text to the total number of words in the text, as a double.
    */
-
+  public static double calculateTicDensity(String[] tics, String fullText) {
+    String[] fullStrings = fullText.split("[\\s\n\t.,?!]+");
+    int count = 0;
+    for (int i=0; i<fullStrings.length; i++) {
+      for (int j=0; j<tics.length; j++) {
+        if (tics[j].toLowerCase().equals(fullStrings[i].toLowerCase())) {
+          count += 1;
+        }
+      }
+    }
+    double proportion = (double) count/fullStrings.length;
+    String proportionStr = String.format("%.2f", proportion);
+    proportion = Double.parseDouble(proportionStr);
+    return proportion;
+  }
     // write the calculateTicDensity function according to the instructions above
 
     
